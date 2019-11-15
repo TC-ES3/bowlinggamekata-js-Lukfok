804486cef66dae3c2360438fccaa5491e3bc4c11
Lucas Pérez Casado <lucas.perezcasado@travelport.com>
2019-11-15 15:51:57 +0100

    commit 24

--- bowling.js
+++ bowling.js
@@ -12,8 +12,14 @@ export default class Game {
         let i = 0;
 
         for (let frame = 0; frame < 10; frame++) {
-            score += rolls[i] + rolls[i + 1];
-            i += 2 ;
+            if (rolls[i] + rolls[i + 1] === 10) {
+                // spare
+                score += 10 + rolls[i + 2];
+                i += 2;
+            } else {
+                score += rolls[i] + rolls[i + 1];
+                i += 2 ;
+            }
         }
         return score;
     }
